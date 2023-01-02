fe1e899fd2f3ff7ce6fd1b51eec1bb9ea152d6c4
SamSchultz98 <sam.schultz@outlook.com>
2023-01-02 15:33:35 -0500

    ProgramCs File Updated With Builder Settings and Framework

--- DansBigBarginBarn/Program.cs
+++ DansBigBarginBarn/Program.cs
@@ -1,6 +1,15 @@
+using DansBigBarginBarn.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(x =>
+{
+    x.UseSqlServer(builder.Configuration.GetConnectionString("Main"));
+});
+
+builder.Services.AddCors();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -8,6 +17,13 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseCors(x =>
+{
+    x.AllowAnyHeader()
+    .AllowAnyMethod()
+    .AllowAnyOrigin();
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
