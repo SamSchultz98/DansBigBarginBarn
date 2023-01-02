c0b24d2a2d01628965cbca2fe26b82c4af1cfc72
SamSchultz98 <sam.schultz@outlook.com>
2023-01-02 15:28:34 -0500

    Add project files.

---
+++ DansBigBarginBarn/Models/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DansBigBarginBarn.Models
+{
+    public class AppDbContext : DbContext 
+    {
+        AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder) { }
+    }
+}
