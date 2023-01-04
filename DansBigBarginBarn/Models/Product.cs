88cccf8cc4e9fbf0dee6edcac1ed028b2636c910
SamSchultz98 <sam.schultz@outlook.com>
2023-01-03 23:39:04 -0500

    AddedCommentsForFrontEnd

--- DansBigBarginBarn/Models/Product.cs
+++ DansBigBarginBarn/Models/Product.cs
@@ -7,10 +7,10 @@ namespace DansBigBarginBarn.Models
        public int Id { get; set; }
 
         [StringLength(80)]
-        public string Name { get; set; }
+        public string Name { get; set; }   
 
         [StringLength(140)]
-        public string? Description { get; set; }
+        public string? Description { get; set; }        //This could inlude anything
 
         [StringLength(250)]
         public string? Photopath { get; set; }
